7ae0540dd7f3f03cd7d7a9e2b93f059ca9e7d3a6
Dmitriy Serdtsev <in19farkt@gmail.com>
2019-05-06 12:16:56 +0300

    Added chinese translations

--- data/profMemberships/index.ts
+++ data/profMemberships/index.ts
@@ -4,7 +4,7 @@ export default [
     membershipName: 'The Enterprise Ethereum Alliance (EEA)',
     description: {
       en: 'The Enterprise Ethereum Alliance (EEA) connects Fortune 500 enterprises, startups, academics, and technology vendors with Ethereum subject matter experts. The EEA is helping to evolve Ethereum into an enterprise-grade technology, providing research and development in a range of areas.',
-      ja: '',
+      zh: '企业以太网联盟（EEA）将500强企业，创业公司，学者和技术供应商与以太坊专家联系到一起。EEA正在帮助将以太坊发展成为企业级技术，在一系列领域展开研究和开发。',
       ko: 'EEA(The Enterprise Ethereum Alliance)기업은 포춘지 선정 500개의 기업과 스타트업 및 학계 그리고 이더리움 관련 전문가로 이루어진 기술공급 업체와 연결되어 있습니다. EEA는 이더리움이 대기업급 수준으로 발달할 수 있도록 다양한 분야의 연구 및 개발을 제공하고 있습니다.',
     },
     icon1x: require('./imgs/EEA@1x.png'),
@@ -14,7 +14,7 @@ export default [
     membershipName: 'Multichain Asset Managers Association (MAMA)',
     description: {
       en: 'MAMA is a trade body, initiated by Melonport AG, which represents asset management companies, investors, technology providers, service providers and ecosystem players interested in working towards a new vision for asset management using blockchain and other supporting decentralized technologies.',
-      ja: '',
+      zh: 'MAMA由Melonport AG创办的贸易机构，为资产管理公司，投资者，技术提供商，服务提供商和生态系统参与者提供服务。帮助他们使用区块链和其他去中心化技术来实现资产管理的新愿景。',
       ko: 'MAMA는 자산 관리 회사, 투자자, 기술 제공자 및 서비스 제공자 및 블럭체인 및 분산 지원 기술등을 이용한 자산 관리를 위한 새로운 비전을 지향하는 에코시스템 회사들을 대표하는 Melonport AG에 의해 설립된 무역 기관입니다.',
     },
     icon1x: require('./imgs/Mama@1x.png'),
--- src/shared/types/models.ts
+++ src/shared/types/models.ts
@@ -1,8 +1,10 @@
+import { Lang } from 'services/i18n/namespace';
+
 export interface IEvent {
   eventName: string;
   link: string;
   location: string;
-  description: string;
+  description: Record<Lang, string>;
   startDate: string;
   finishDate?: string;
   image1x: string;

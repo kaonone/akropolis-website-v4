881d8c43625e1955f4b4ba6b509132a22c862f35
Dmitriy Serdtsev <in19farkt@gmail.com>
2019-11-09 10:16:46 +0300

    Fixed error in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,12 @@
 module.exports = {
-  setupFilesAfterEnv: '<rootDir>/jest.setup.ts',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
   snapshotSerializers: ['enzyme-to-json/serializer'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/generators/',
-  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/generators/'],
 
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleDirectories: ['node_modules', 'src'],
@@ -17,16 +14,13 @@ module.exports = {
 
   // This fixed an error related to the CSS and loading gif breaking
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|scss|css)$': '<rootDir>/src/core/__mocks__/fileMock.ts',
-    'ContainersProvider\.tsx$': '<rootDir>/src/core/__mocks__/fileMock.ts',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|scss|css)$':
+      '<rootDir>/src/core/__mocks__/fileMock.ts',
+    'ContainersProvider.tsx$': '<rootDir>/src/core/__mocks__/fileMock.ts',
   },
 
   mapCoverage: true,
   coverageDirectory: '<rootDir>/coverage',
-  collectCoverageFrom: [
-    '**/src/**/*.{ts,tsx}',
-    '!**/loader.{ts,tsx}',
-    '!**/node_modules/**',
-  ],
+  collectCoverageFrom: ['**/src/**/*.{ts,tsx}', '!**/loader.{ts,tsx}', '!**/node_modules/**'],
   coveragePathIgnorePatterns: ['.*\\.d\\.ts'],
 };

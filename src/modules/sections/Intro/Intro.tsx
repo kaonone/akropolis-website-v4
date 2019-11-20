8c2cb76ec0401ed5f10029a4c6f474a1b42307d6
Dmitriy Serdtsev <in19farkt@gmail.com>
2019-11-20 19:26:21 +0300

    Remove video from Intro component

--- src/modules/sections/Intro/Intro.tsx
+++ src/modules/sections/Intro/Intro.tsx
@@ -1,73 +1,17 @@
 import * as React from 'react';
 
 import { useTranslate } from 'services/i18n';
-import { Button, Modal } from 'shared/view/elements';
 import { StylesProps, provideStyles } from './Intro.style';
-import { Lang } from 'services/i18n/namespace';
-import { Adaptive } from 'services/adaptability';
-
-const videoByLang: Record<Lang, string> = {
-  en: 'https://www.youtube.com/embed/WVWxCgzPihc',
-  ko: 'https://www.youtube.com/embed/y7Go1mVqjs0',
-  vi: 'https://www.youtube.com/embed/C45MWDqUIcA',
-  zh: 'https://www.youtube.com/embed/VMFUNDEYRQg',
-};
 
 function Intro(props: StylesProps) {
-  const { classes, theme } = props;
+  const { classes } = props;
   const { t, tKeys } = useTranslate();
-  const [isOpened, setIsOpened] = React.useState(false);
+
   return (
     <div className={classes.root}>
       <p className={classes.title}>{t(tKeys.sections.intro.title.getKey())}</p>
       <p className={classes.subtitle}>{t(tKeys.sections.intro.subtitle.getKey())}</p>
-      <Button
-        className={classes.button}
-        variant="contained"
-        color="primary"
-        size="large"
-        onClick={setIsOpened.bind(null, true)}
-      >
-        {t(tKeys.sections.intro.watchVideoButton.getKey())}
-      </Button>
-      <Modal
-        open={isOpened}
-        onClose={setIsOpened.bind(null, false)}
-        classes={{ root: classes.modalRoot }}
-        BackdropProps={{
-          classes: {
-            root: classes.backDropRoot,
-          },
-        }}
-      >
-        <>
-          <Adaptive to="md">
-            <Video width={theme ? theme.breakpoints.values.sm : 0} />
-          </Adaptive>
-          <Adaptive from="md" to={920}>
-            <Video width={theme ? theme.breakpoints.values.md : 0} />
-          </Adaptive>
-          <Adaptive from={920}>
-            <Video width={920} />
-          </Adaptive>
-        </>
-      </Modal>
-    </div >
-  );
-}
-
-function Video({ width }: { width: number }) {
-  const { locale } = useTranslate();
-  return (
-    <iframe
-      style={{ maxWidth: '100vw' }}
-      width={width}
-      height={width * 0.5625}
-      src={videoByLang[locale]}
-      frameBorder={0}
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    />
+    </div>
   );
 }
 

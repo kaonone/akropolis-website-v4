9c8212d7bd32b0efbca1d1d5d2aeab6ef51de91c
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-06-15 17:15:14 +0300

    Pin light theme for wiki pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,29 @@
 import * as React from 'react';
 import { Switch, Route, Redirect } from 'react-router';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
 import routes from 'modules/routes';
-import { Main } from './pages/Main/Main';
-
 import { Partners, News } from 'modules/sections';
+import { lightTheme } from 'shared/styles/theme';
+
+import { Main } from './pages/Main/Main';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export function App() {
   return (
     <Switch>
       <Route exact path="/" component={Main} />
       <Route exact key="ForWikiPartners" path={routes.forWiki.partners.getRoutePath()}>
-        <Partners withTitle={false} />
+        <MuiThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Partners withTitle={false} />
+        </MuiThemeProvider>
       </Route>
       <Route exact key="ForWikiNews" path={routes.forWiki.news.getRoutePath()}>
-        <News withTitle={false} withPagination={false} />
+        <MuiThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <News withTitle={false} withPagination={false} />
+        </MuiThemeProvider>
       </Route>
       <Redirect to="/" />
     </Switch>
--- src/modules/ForWiki/ForWiki.tsx
+++ src/modules/ForWiki/ForWiki.tsx
@@ -1,26 +1,20 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
-import { MuiThemeProvider } from '@material-ui/core/styles';
 
 import { IModule } from 'shared/types/app';
 import routes from 'modules/routes';
-import { lightTheme } from 'shared/styles/theme';
 import { Partners, News } from '../sections';
 
 const ForWikiModule: IModule = {
   getRoutes() {
     return [(
-      <MuiThemeProvider theme={lightTheme}>
-        <Route exact key="ForWikiPartners" path={routes.forWiki.partners.getRoutePath()} >
-          <Partners withTitle={false} />
-        </Route>
-      </MuiThemeProvider>
+      <Route exact key="ForWikiPartners" path={routes.forWiki.partners.getRoutePath()} >
+        <Partners withTitle={false} />
+      </Route>
     ), (
-      <MuiThemeProvider theme={lightTheme}>
-        <Route exact key="ForWikiNews" path={routes.forWiki.news.getRoutePath()} >
-          <News withTitle={false} withPagination={false} />
-        </Route>
-      </MuiThemeProvider>
+      <Route exact key="ForWikiNews" path={routes.forWiki.news.getRoutePath()} >
+        <News withTitle={false} withPagination={false} />
+      </Route>
     )];
   },
 };

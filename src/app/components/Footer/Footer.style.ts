7833233e29d1e8e203c5fb231b3ed76c463213c7
Artem Lagunov <lagunov.artamon@gmail.com>
2020-06-29 19:01:38 +0300

    styled footer for 1440 resolution

--- src/app/components/Footer/Footer.style.ts
+++ src/app/components/Footer/Footer.style.ts
@@ -1,41 +1,58 @@
 import { makeStyles } from 'shared/styles';
 
-export const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
-
-    flexWrap: 'wrap',
-    [theme.breakpoints.up('tabletXS')]: {
-      flexWrap: 'nowrap',
-      justifyContent: 'space-between',
-    },
+    justifyContent: 'space-between',
   },
 
-  address: {
-    width: '100%',
-    opacity: 0.3,
-
-    marginBottom: theme.spacing(3.75),
-    lineHeight: 1.6,
-    fontSize: theme.spacing(1.25),
-    [theme.breakpoints.up('tabletXS')]: {
-      width: 'unset',
-      maxWidth: theme.spacing(62),
-      marginBottom: 0,
-      lineHeight: 1.33,
-      fontSize: theme.spacing(1.5),
-    },
-    [theme.breakpoints.up('desktopXS')]: {
-      lineHeight: 'unset',
-      maxWidth: 'unset',
-    },
+  copyright: {
+    display: 'flex',
+    flexDirection: 'column',
+    fontSize: '12px',
+    lineHeight: 'normal',
   },
 
   nav: {
-    width: '100%',
-    [theme.breakpoints.up('tabletXS')]: {
-      width: 'unset',
-      marginLeft: theme.spacing(2),
+    marginBottom: '12px',
+
+    '& $link': {
+      color: '#000',
+      textDecoration: 'none',
+
+      '& + $link': {
+        marginLeft: '50px',
+      },
     },
   },
+
+  text: {
+    opacity: 0.3,
+  },
+
+  partners: {
+    display: 'flex',
+
+    '& $link': {
+      fontSize: 0,
+
+      '&:last-child': {
+        marginLeft: '50px',
+      }
+    },
+  },
+
+  messari: {
+    width: '119px',
+    height: '40px'
+  },
+
+  etherium: {
+    width: '160px',
+    height: '40px'
+  },
+
+  link: {},
 }));
+
+export { useStyles };
--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,25 +1,60 @@
 import React from 'react';
 
-import { useStyles } from './Footer.style';
-import { FooterNavigation } from './FooterNavigation/FooterNavigation';
+import { tKeys, useTranslate } from 'services/i18n';
+import { PRIVACY_POLICY_URL, T_AND_C_URL } from 'assets';
 
-export function Footer() {
+import { Messari, Etherium } from './Icons';
+import { useStyles } from './Footer.style';
+
+function Footer() {
   const classes = useStyles();
+  const { t } = useTranslate();
 
   const startYear = 2017;
   const yearNow = new Date().getFullYear();
 
   return (
     <footer className={classes.root}>
-      <small className={classes.address}>
-        {[
-          'Akropolis Decentralised Ltd.',
-          'Suite 23 Portland House, Glacis Road, Gibraltar, GX11 1AA',
-          'COMPANY NUMBER: 116430',
-          `${startYear}${yearNow > startYear ? '-' + yearNow : ''}, All right reserved`,
-        ].join(' | ')}
-      </small>
-      <FooterNavigation className={classes.nav} />
+      <div className={classes.copyright}>
+        <nav className={classes.nav}>
+          <a
+            className={classes.link}
+            href={PRIVACY_POLICY_URL}
+            target="_blank"
+            title={t(tKeys.modules.navigation.privacyPolicy.getKey())}
+          >
+            Privacy Policy
+          </a>
+          <a
+            className={classes.link}
+            href={T_AND_C_URL}
+            target="_blank"
+            title={t(tKeys.modules.navigation.termsConditions.getKey())}
+          >
+            Terms & Conditions
+          </a>
+        </nav>
+        <div className={classes.text}>
+          {[
+            'Akropolis Decentralised Ltd.',
+            'Suite 23 Portland House, Glacis Road, Gibraltar, GX11 1AA',
+            'COMPANY NUMBER: 116430',
+            `${startYear}${yearNow > startYear ? '-' + yearNow : ''}, All right reserved`,
+          ].join(' | ')}
+        </div>
+      </div>
+      <div className={classes.partners}>
+        <a className={classes.link} href="https://messari.io/asset/akropolis" target="_blank" rel="noopener noreferrer">
+          Messari
+          <Messari className={classes.messari} />
+        </a>
+        <a className={classes.link} href="https://ethereum.org" target="_blank" rel="noopener noreferrer">
+          Etherium
+          <Etherium className={classes.etherium} />
+        </a>
+      </div>
     </footer>
   );
 }
+
+export { Footer };

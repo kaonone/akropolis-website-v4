0da030a3c1d2aaf22ebcbea814fd78b9b65403e2
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-08-12 16:31:48 +0300

    Add platform privacy policy

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,4 @@
 export { default as PRIVACY_POLICY_URL } from 'assets/Akropolis_Privacy_Policy.pdf';
 export { default as T_AND_C_URL } from 'assets/Akropolis_Terms_and_Conditions.pdf';
+
+export { default as PLATFORM_PRIVACY_POLICY_URL } from 'assets/Akropolis_Platform_Privacy_Policy.pdf';

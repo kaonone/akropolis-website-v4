80d84fe4b75ca7e73cc3f5863425a077c4c971f4
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-08-14 19:12:18 +0300

    Add token swap t&c

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,3 +4,6 @@ export { default as T_AND_C_URL } from 'assets/Akropolis_Terms_and_Conditions.pd
 // Don't delete! It is used in Sparta and Delphi
 export { default as PLATFORM_PRIVACY_POLICY_URL } from 'assets/Akropolis_Platform_Privacy_Policy.pdf';
 export { default as PLATFORM_T_AND_C_URL } from 'assets/Akropolis_Platform_Terms_of_Service.pdf';
+
+// for history
+export { default as TOKEN_SWAP_T_AND_C_URL } from 'assets/Akropolis_Terms_and_Conditions_Token_Swap.pdf';

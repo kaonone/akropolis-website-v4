3738c8241bbc8bcb92416c3b9204a10cedb405ce
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-09-21 15:29:03 +0300

    Update roadmap link

--- src/app/components/Header/constants.ts
+++ src/app/components/Header/constants.ts
@@ -8,7 +8,7 @@ export const menuItems: IMenuItem[] = [
     isExternal: true,
   },
   {
-    path: 'https://www.notion.so/d6f645ed6e1f4044b4b8777cb96ed433',
+    path: 'https://www.notion.so/b390a85d87124796b0b008b20e6ab38e?v=387bc54a16e34bf2b8f7328b839eebc2',
     title: tKeys.modules.navigation.roadmap.getKey(),
     isExternal: true,
   },

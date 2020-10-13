8cd89a280cc2fd877a8418ebabc6306590690f73
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-10-13 17:59:02 +0300

    Update SUPPORT_EMAIL

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,4 +1,4 @@
 export const LOCALE_STORAGE_KEY = '__akro_locale__';
-export const SUPPORT_EMAIL = 'support@akropolis.io';
+export const SUPPORT_EMAIL = 'team@akropolis.io';
 export const WHITE_SPACE = '\u00A0';
 export const RECAPTCHA_SITE_KEY = '6Ld31qgUAAAAAKbw86QqitirAhBDVw4BP_cCVDBG';
